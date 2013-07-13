1cbd869665feec506d2f93834c15683f7d2c3cc9
Ubuntu <ubuntu@ip-172-31-46-199.us-west-2.compute.internal>
2013-07-13 08:17:59 +0000

    Read web content from file index.html

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var infile = "index.html";
+var indxBuff = fs.readFileSync(infile);
+var indxStr = indxBuff.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+  response.send(indxStr);
 });
 
 var port = process.env.PORT || 5000;
